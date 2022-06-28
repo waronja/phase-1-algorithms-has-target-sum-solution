10660b5491a9a5faab0a1b3f4070031a4ad98ea2
simon waronja <simon.waronja@student.moringaschool.com>
2022-06-28 12:30:23 +0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-function hasTargetSum(array, target) {
+function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
   Add your pseudocode here
+  
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+  if we reach the middle, and all the letters match, return true
 */
 
 /*
   Add written explanation of your solution here
+  return true if the letter we're iterating over to the corresponding letter at the end of the string
+  match and false if they don't
 */
 
 // You can run `node index.js` to view these console logs
